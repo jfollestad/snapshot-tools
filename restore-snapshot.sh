b4febb9b5e9a64d847359b9822ec8fbcf91076ab
jfollestad <jfollestad@outlook.com>
2019-10-30 13:21:13 +0100

    added restore and shrink

--- restore-snapshot.sh
+++ restore-snapshot.sh
@@ -1,17 +1,1 @@
 #!/bin/bash
-
-inputfile=$1
-outputfile=$2
-
-disksize=$(sudo blockdev --getsize64 $inputfile)
-echo "Disc size is: $disksize"
-
-#usedsize=$(sudo du -sb $inputfile | awk '{print $1}')
-#echo "Used size is: ${usedsize}B"
-
-#sudo dd if=$inputfile | pv -s $disksize | dd of=$outputfile
-#sudo dd bs=4096 if=$inputfile | pv -s $disksize | sudo gzip -9 > $outputfile
-
-sudo dd if=$inputfile of=$outputfile status=progress
-
-sudo chown $USER:$USER $outputfile
