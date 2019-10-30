1f112e459503749def1f11414ef9db911bc9ee54
jfollestad <jfollestad@outlook.com>
2019-10-30 13:17:35 +0100

    added create-snapshot.sh

--- create-snapshot.sh
+++ create-snapshot.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 
-inputfile=$1
-outputfile=$2
+input=$1
+output=$2
 
-disksize=$(sudo blockdev --getsize64 $inputfile)
+disksize=$(sudo blockdev --getsize64 $input)
 echo "Disc size is: $disksize"
 
-#usedsize=$(sudo du -sb $inputfile | awk '{print $1}')
-#echo "Used size is: ${usedsize}B"
+blocksize=$(sudo blockdev --getbsz $input)
+echo "Blocksize on input is: $blocksize"
 
-#sudo dd if=$inputfile | pv -s $disksize | dd of=$outputfile
-#sudo dd bs=4096 if=$inputfile | pv -s $disksize | sudo gzip -9 > $outputfile
+sudo dd bs=$blocksize if=$input | pv -s $disksize | dd of=$output
+#sudo dd bs=4096 if=$input | pv -s $disksize | sudo gzip -9 > $output
+#sudo dd if=$input of=$output status=progress
 
-sudo dd if=$inputfile of=$outputfile status=progress
-
-sudo chown $USER:$USER $outputfile
+sudo chown $USER:$USER $output
