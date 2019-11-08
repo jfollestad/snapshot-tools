15f39390c5c465c2d698669e3b74be4bd0c2e8a9
jfollestad <jfollestad@outlook.com>
2019-11-08 23:27:42 +0100

    cahnges

--- sandbox/snippets-snapshot_shrink.sh
+++ sandbox/snippets-snapshot_shrink.sh
@@ -9,3 +9,12 @@ mount -o loop image.img /mnt/ChromeOS/
 
 # Clear empty space
 dd if=/dev/zero of=bigfile bs=1M
+
+
+
+	#part_dump=$(mktemp)
+	#sfdisk -d $dev > $part_dump
+	#p2_finish_current=$(cat $part_dump | tail -1 | awk '{print $6}' | tr -d ',')
+	#sed -i "s/$p2_finish_current/$p2_finish/g" $part_dump
+
+sudo sfdisk -s /dev/mmcblk1
